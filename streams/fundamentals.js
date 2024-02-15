0b19ca4f4809dbd9784754d41d81be0b65e4dc77
EngJao89 <eng_joao@outlook.com>
2024-02-15 10:55:09 -0300

    feat(f01-write-stream): write stream implementation

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,6 +1,6 @@
-import { Readeble} from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
-class OneToHundredStream extends Readeble {
+class OneToHundredStream extends Readable {
   index = 1
   _read() {
     const i = this.index++
@@ -17,4 +17,21 @@ class OneToHundredStream extends Readeble {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
